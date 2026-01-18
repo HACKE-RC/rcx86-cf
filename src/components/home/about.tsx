97ecbb0821526cb9f325f558bad358a22a71acf7
HACKE-RC <60568652+HACKE-RC@users.noreply.github.com>
2026-01-18 23:17:37 +0530

    clear: fix

--- src/components/home/about.tsx
+++ src/components/home/about.tsx
@@ -8,7 +8,6 @@ const About = () => {
   <div className="about-me text-muted-foreground flex flex-col gap-4">
     <p >
     I love building, breaking and working with low-level computer science. My main interests are in systems programming.
-    Currently, researching agents for security @ <Link title='Rexion' href={'https://rexion.ai/'} className='text-foreground hover:underline'> Rexion </Link> 
     </p>
     <p>
     Magnificent Grants Grantee '25.
@@ -19,4 +18,5 @@ const About = () => {
   )
 }
 
+//    Currently, researching agents for security @ <Link title='Rexion' href={'https://rexion.ai/'} className='text-foreground hover:underline'> Rexion </Link> 
 export default About
