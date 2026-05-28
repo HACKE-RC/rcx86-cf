3e0da336194ea641dc3134d29a621ed6b6435190
HACKE-RC <60568652+HACKE-RC@users.noreply.github.com>
2026-05-28 16:03:04 +0530

    Update site title metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,19 @@
-import type { Metadata } from "next"
-import { Host_Grotesk } from "next/font/google"
-import "./globals.css"
-import { ThemeProvider } from "@/components/providers/theme-provider"
-import { home_page_data } from "@/data/home"
-import ReactQueryProvider from "@/components/providers/tanstack-query-provider"
+import type { Metadata } from "next";
+import { Host_Grotesk } from "next/font/google";
+import "./globals.css";
+import ReactQueryProvider from "@/components/providers/tanstack-query-provider";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { home_page_data } from "@/data/home";
 
 const font = Host_Grotesk({
   subsets: ["latin", "latin-ext"],
   display: "swap",
-})
+});
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://rcx86.me"), // change later
 
-  title: {
-    default: "rcx86 — Low-Level Computer Science & Systems Engineering",
-    template: "%s · rcx86",
-  },
+  title: "Mr. Rc",
 
   description: home_page_data.bio,
 
@@ -55,22 +52,15 @@ export const metadata: Metadata = {
   openGraph: {
     type: "website",
     siteName: "rcx86",
-    title: "rcx86 — Low-Level Computer Science & Systems Engineering",
+    title: "Mr. Rc",
     description: home_page_data.bio,
     url: "https://rcx86.me",
-    images: [
-      {
-        url: "/og.png",
-        width: 1200,
-        height: 630,
-        alt: "rcx86 — Low-Level Computer Science Research & Engineering",
-      },
-    ],
+    images: [{ url: "/og.png", width: 1200, height: 630, alt: "Mr. Rc" }],
   },
 
   twitter: {
     card: "summary_large_image",
-    title: "rcx86 — Low-Level Computer Science & Systems Engineering",
+    title: "Mr. Rc",
     description: home_page_data.bio,
     creator: "@rcx86",
     images: ["/og.png"],
@@ -92,29 +82,29 @@ export const metadata: Metadata = {
   ],
 
   category: "technology",
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${font.className} bg-zinc-100 dark:bg-neutral-900 antialiased`}
       >
-      <ReactQueryProvider>
+        <ReactQueryProvider>
           <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </ReactQueryProvider>
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </ReactQueryProvider>
       </body>
     </html>
-  )
+  );
 }
